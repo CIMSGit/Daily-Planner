dc099e0aceca419c7eff855cdf385e5be57a4289
CIMSGit <117454891+CIMSGit@users.noreply.github.com>
2023-01-20 23:27:14 +0000

    adds input area

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let currentDate =$("#currentDay");
-let currentMoment =$("<p>");
+let currentDate = $("#currentDay");
+let currentMoment = $("<p>");
 currentMoment.text((moment().format("dddd, MMMM Do YYYY")));
 currentDate.append(currentMoment)
+
+let textArea = $(".col");
+let tasks = $("<TEXTAREA>");
+tasks.text("");
+textArea.append(tasks);
+
+
+
